adb1d72ee9255cd3446fcb1fb1661650367dd8be
Ivan Putera Pratama <ivanputeraa@gmail.com>
2016-12-23 07:43:29 +0700

    Menambahkan fitur untuk list wilayah

--- resources/views/admin/wilayah.blade.php
+++ resources/views/admin/wilayah.blade.php
@@ -10,4 +10,35 @@
 	<a href="{{ URL::to('admin/wilayah/create') }}<?php echo "?id=$request->id"; ?>" class="btn btn-default"><span class="glyphicon glyphicon-plus"></span>Tambah Wilayah Baru</a>
 </div>
 </div>
+
+<div id="table">
+<table class="table table-hover">
+
+<th>ID</th>
+<th>Nama Wilayah</th>
+<th>Info</th>
+<th>Terakhir diupdate</th>
+<th>Prediksi</th>
+<th>Terakhir diupdate</th>
+<th>Action</th>
+@if($stories->isEmpty())
+	<p>Belum ada wilayah</p>
+@else
+@foreach($stories as $story)
+<tr>
+	<td>{{$story->id}}</td>
+	<td>{{$story->nama}}</td>
+	<td><a href="{{ URL::to('admin/wilayah/'.$story->id.'/info') }}">Klik disini untuk update</a></td>
+	<td>{{$story->info->updated_at}}</td>
+	<td><a href="{{ URL::to('admin/wilayah/'.$story->id.'/prediksi') }}">Klik disini untuk update</a></td>
+	<td>{{$story->prediksi->updated_at}}</td>
+	<td>
+	<a href="{{ URL::to('admin/wilayah/'.$story->id.'/edit') }}"><span class="glyphicon glyphicon-pencil"></span></a>
+	<a href="{{ URL::to('admin/wilayah/'.$story->id.'/delete') }}"><span class="glyphicon glyphicon-trash"></span></a>
+	</td>
+</tr>
+@endforeach
+@endif
+</table>
+</div>
 @stop
